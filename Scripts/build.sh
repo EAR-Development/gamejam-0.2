1b58f439631439d27bee720d0e17aaf40c4b255e
Alexander Engelsberger <alex@ear-dev.de>
2016-10-02 18:44:11 +0200

    Release/bootstrap (#1)
    * Travis Setup
    
    * Travis: correct Permission
    
    * Travis: Work 1
    
    * Travis : Work 2
    
    * Travis: Work 3
    
    * Travis: Correct URL
    
    * Travis: Upload
    
    * Travis: Clean Up
    
    * Travis Test2
    
    * Travis correct URLS
    
    * Travis correct URL
    
    * Travis Deploy

---
+++ Scripts/build.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# Change this the name of your project. This will be the name of the final executables as well.
+project="gamejam-0.2"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+    -batchmode \
+      -nographics \
+        -silent-crashes \
+          -logFile $(pwd)/unity.log \
+            -projectPath $(pwd) \
+              -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+                -quit
+
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+    -batchmode \
+      -nographics \
+        -silent-crashes \
+          -logFile $(pwd)/unity.log \
+            -projectPath $(pwd) \
+              -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+                -quit
+
+
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+    -batchmode \
+      -nographics \
+        -silent-crashes \
+          -logFile $(pwd)/unity.log \
+            -projectPath $(pwd) \
+              -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+                -quit
+
+cd build
+
+hdiutil create -fs HFS+ -srcfolder "osx" -volname "gamejam-0.2-osx" "gamejam-0.2-osx.dmg"
+zip -r "gamejam-0.2-windows.zip" "windows"
+zip -r "gamejam-0.2-linux.zip" "linux"
