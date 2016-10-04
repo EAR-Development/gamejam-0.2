ae994ffce7513b98ae6027511d52b5cf7bd111f8
Alexander Engelsberger <alex@ear-dev.de>
2016-10-04 10:29:10 +0200

    Add Pause Menu

---
+++ Assets/scripts/gameControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class gameControllerScript : MonoBehaviour {
+
+	public Canvas pausemenu;
+
+	// Use this for initialization
+	void Start () {
+		pausemenu = pausemenu.GetComponent<Canvas> ();
+		pausemenu.enabled = false;
+	}
+
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			EscapeKeyPressed ();
+		}
+	}
+
+
+	public void EscapeKeyPressed(){
+		pausemenu.enabled = ! pausemenu.enabled;
+	}
+
+	public void ExitButtonPressed(){
+		SceneManager.LoadScene ("scenes/menu");
+	}
+
+	public void ResumeButtonPressed(){
+		pausemenu.enabled = false;
+	}
+}
