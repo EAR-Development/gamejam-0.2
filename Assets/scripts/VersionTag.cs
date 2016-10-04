7eef3fb8c2fe93c93092693fc58c4d5cc557faf0
Alexander Engelsberger <alex@ear-dev.de>
2016-10-04 09:38:09 +0200

    Main Menu

---
+++ Assets/scripts/VersionTag.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VersionTag : MonoBehaviour {
+	string VERSION = "Developer Build";
+
+	public bool ShowVersionInformation = false;
+	public bool ShowVersionDuringTheFirst20Seconds = true;
+	Rect position = new Rect (0, 0, 100, 20);
+
+
+	void Start ()
+	{
+		DontDestroyOnLoad (this);
+
+		if (ShowVersionDuringTheFirst20Seconds) {
+			ShowVersionInformation = true;
+			Destroy (this, 20f);
+		}
+
+		position.x = 10f;
+		position.y = Screen.height - position.height - 10f;
+
+	}
+
+
+	void OnGUI ()
+	{
+		if (!ShowVersionInformation) {
+			return;
+		}
+
+		GUI.contentColor = Color.white;
+		GUI.Label (position, VERSION);
+	}
+}
---
+++ Assets/scripts/menuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+
+		quitMenu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ExitPress(){
+		quitMenu.enabled = true;
+
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress(){
+		quitMenu.enabled = false;
+
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartPress(){
+		SceneManager.LoadScene ("scenes/main");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
+
+
