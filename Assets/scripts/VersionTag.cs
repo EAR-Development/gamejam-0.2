cabb085faf9b200d7a524e5f627d6a7bef9a0253
Alexander Engelsberger <alex@ear-dev.de>
2016-10-04 09:52:40 +0200

    remove background image

--- Assets/scripts/VersionTag.cs
+++ Assets/scripts/VersionTag.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 
 public class VersionTag : MonoBehaviour {
-	string VERSION = "Developer Build";
+	private string VERSION;
+	#if UNITY_EDITOR
+	void setVersion(){
+		VERSION = "developer build";
+	}
+	#else
+	void setVersion(){
+		VERSION = "0.0.01";
+	}
+	#endif
 
 	public bool ShowVersionInformation = false;
 	public bool ShowVersionDuringTheFirst20Seconds = true;
 	Rect position = new Rect (0, 0, 100, 20);
 
 
+
 	void Start ()
 	{
+		setVersion ();
 		DontDestroyOnLoad (this);
 
 		if (ShowVersionDuringTheFirst20Seconds) {
@@ -20,6 +31,7 @@ public class VersionTag : MonoBehaviour {
 
 		position.x = 10f;
 		position.y = Screen.height - position.height - 10f;
+
 
 	}
 
