85a9db178bc996721259f7467ada105880b34ec1
Alexander Engelsberger <theraedrummer@gmail.com>
2016-10-06 15:30:49 +0200

    Hauptmenu

---
+++ Assets/scripts/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	//Otherwise you can do it publicly.  
+	public Texture2D cursor;
+
+	public void OnPointerEnter()
+	{
+		Cursor.SetCursor (cursor, Vector2.zero, CursorMode.Auto);
+	}
+
+	public void OnPointerExit()
+	{
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+
+}
